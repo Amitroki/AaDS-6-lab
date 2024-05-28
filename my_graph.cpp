9bd73ffb3f53f0fc45fdafb7b2b78935694f57fa
Алексей Русяев <RusyaevAA@outlook.com>
2024-05-29 02:40:40 +0400

    added functions for adding vertex, removing vertex, checking for the presence of the vertex, printing the vector which contains all vertices and getting this vector for user

--- my_graph.cpp
+++ my_graph.cpp
@@ -4,21 +4,58 @@
 using namespace std;
 
 namespace own_graph {
-	template<typename T>
+	template<typename Vertex, typename Distance = double>
 	struct Edge {
-		T start;
-		T end;
-		double weight;
-		Edge(size_t first, size_t second, double way = 0): start(first), end(second) {
-			if (way != 0) weight = way;
+		Vertex start;
+		Vertex end;
+		Distance weight;
+		Edge(Vertex first, Vertex second, Distance way = 0): start(first), end(second) {
+			if (way != 0) 
+				weight = way;
 		}
 	};
-	template<typename T>
+	template<typename Vertex, typename Distance = double>
 	class Graph {
 	private:
-		vector<T> _vertices;
-		vector<Edge<T>> _edges;
+		vector<Vertex> _vertices;
+		vector<Edge<Vertex, Distance>> _edges;
 	public:
-		Graph(): _vertices(vector<T>), _edges(vector<Edge<T>>) {}
+		Graph(): _vertices(vector<Vertex>()), _edges(vector<Edge<Vertex, Distance>>()) {}
+
+		bool has_vertex(const Vertex& vertex) const {
+			for (size_t i = 0; i < _vertices.size(); i++) {
+				if (_vertices[i] == vertex) 
+					return true;
+			}
+			return false;
+		}
+
+		void add_vertex(const Vertex& vertex) {
+			_vertices.push_back(vertex);
+		}
+
+		bool remove_vertex(const Vertex& vertex) {
+			if (has_vertex(vertex)) {
+				for (size_t i = 0; i < _vertices.size(); i++) {
+					if (_vertices[i] == vertex) {
+						_vertices.erase(_vertices.begin() + i);
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		vector<Vertex> vertices() const {
+			return _vertices;
+		}
+
+		void print_vertices() const {
+			cout << "vertices in this graph:\t{";
+			for (size_t i = 0; i < _vertices.size(); i++) {
+				cout << ' ' << _vertices[i];
+			}
+			cout << " }" << endl;
+		}
 	};
 }
--- tests_graph.cpp
+++ tests_graph.cpp
@@ -19,5 +19,34 @@ TEST(EdgeTest, CheckingTheSetValues3) {
 	Edge<int> a(-1, 2, -9.0);
 	EXPECT_EQ(a.start, -1);
 	EXPECT_EQ(a.end, 2);
-	EXPECT_NE(a.weight, -9.0);
+	EXPECT_EQ(a.weight, -9.0);
+}
+
+TEST(GraphTest, CheckingTheSetValues1) {
+	Graph<int> a;
+	int vertex = 0;
+	a.add_vertex(vertex);
+	EXPECT_TRUE(a.has_vertex(0));
+	vertex = 9;
+	a.add_vertex(vertex);
+	EXPECT_FALSE(a.has_vertex(10));
+	EXPECT_FALSE(a.has_vertex(90));
+}
+TEST(GraphTest, CheckingTheSetValues2) {
+	Graph<int> a;
+	int vertex = 0;
+	a.add_vertex(vertex);
+	EXPECT_TRUE(a.has_vertex(0));
+	vertex = 9;
+	a.add_vertex(vertex);
+	EXPECT_FALSE(a.has_vertex(10));
+	EXPECT_FALSE(a.has_vertex(90));
+	vertex = 90;
+	a.add_vertex(vertex);
+	EXPECT_TRUE(a.has_vertex(90));
+	a.remove_vertex(vertex);
+	EXPECT_FALSE(a.has_vertex(90));
+	a.print_vertices();
+	a.add_vertex(10);
+	a.print_vertices();
 }
